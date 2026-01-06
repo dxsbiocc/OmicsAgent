7e810ac9a20b9ed9fdcafcc753c222d5cd0d8861
dengxsh <dxsbiocc@gmail.com>
2026-01-06 10:07:44 +0800

    2026 First major update

--- Biomni-main/biomni_env/new_software_v006.sh
+++
@@ -1,4 +0,0 @@
-# Biomni v0.0.6 - Incremental Software Installation
-# Add any new packages/software introduced in version 0.0.6 below
-pip install transformers sentencepiece langchain-google-genai langchain_ollama mcp
-pip install lazyslide
